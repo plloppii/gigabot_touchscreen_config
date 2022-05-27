f9df4bbd86dd107597c00439aafd51fd93ae2911
Noah Pan <noahpan323@gmail.com>
2022-05-26 21:48:43 -0500

    Add first boot option to reload to sleep script

--- klipper/reload.py
+++ klipper/reload.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
-import requests
-import json
-import os
-import subprocess
+import sys, requests, json, os, subprocess, configparser, time
+from pathlib import Path
+
+if len(sys.argv) > 1 and sys.argv[1] == "--first-boot":
+    time.sleep(5)
 
 url = "http://localhost"
-klipper_path = "/home/pi/klipper_config/"
-klipper_scripts = klipper_path + "scripts/"
+home_path = Path(__file__).parent.resolve().parent.parent
+klipper_path = home_path / "klipper"
+moonraker_path = home_path / "moonraker"
+
+klipper_config_path = home_path / "klipper_config"
+klipper_config_scripts = klipper_config_path / "scripts"
+master_config_path = klipper_config_path / ".master.cfg"
+
+#Reloading serial script, Import script directly to run it.
+if (klipper_config_scripts / "setup_printer.py").is_file():
+    sys.path.append(str(klipper_config_scripts))
+    import setup_printer
+    setup_printer.main()
+else:
+    print("Could not locate setup_printer.py in {}!".format(klipper_config_path))
 
 #Reloading UI configuration
-default_json_file = klipper_path + ".theme/default.json"
+default_json_file = klipper_config_path / ".theme/default.json"
 with open(default_json_file) as f:
     defaults = json.load(f)
 namespaces = [name for name in defaults]
@@ -28,8 +42,16 @@ for name in namespaces:
     post_response = requests.post(post_url, json=body)
     print(post_response.text)
 
-#Reloading serial script
-serial_out = subprocess.run([klipper_scripts + "get_serial.sh"], capture_output=True)
-print(serial_out)
+master_config = configparser.ConfigParser(inline_comment_prefixes="#")
+master_config.read(str(master_config_path))
+printer_config = master_config["re3D"]
+
+branch = printer_config.get("branch", "stable")
+
+#Manage Moonraker, Klipper, virtual_keyboard version, based on klipper_config hashes
+#Fetch update manager status endpoint /machine/update/status?refresh=true
+
+#pull in klipper_config master.cfg, see if branch is on stable or develop, 
+# check out Moonraker, Klipper, klipper_config develop branches if set to develop
 
 os.system("service moonraker restart")
